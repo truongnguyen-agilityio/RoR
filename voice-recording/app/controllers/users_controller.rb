fbcc9028ed54ff5c079e02a6f121c0521e5a9d7f
Truong Nguyen <truong.nguyenxuan@asnet.com.vn>
2015-04-24 14:19:24 +0700

    refactor code edit audio of user

--- voice-recording/app/controllers/users_controller.rb
+++ voice-recording/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :set_user, only: [:show, :edit, :update]
   def index
     @users = User.all
   end
@@ -6,32 +7,40 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.json { render json: {id: @user.id, success: true}}
-        # render json: { user: @user, status: :created}
-        # format.json { user: @user, status: :created}
-        # format.html { redirect_to user_path(@user), notice: 'User was successfully created.' }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
+    if @user.save
+      render json: {id: @user.id, success: true}
+    else
+      render json: {id: @user.id, success: false}
     end
+    # end
   end
 
   def new
     @user = User.new
   end
 
+  # GET /user/1/edit
+  def edit
+  end
+
   def update
+    @user.assign_attributes(user_params)
+    if @user.save
+      render json: {id: @user.id, success: true}
+    else
+      render json: {id: @user.id, success: false}
+    end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
     def user_params
       params.permit(:email, :password, :audio)
+    end
+    def set_user
+      @user = User.find(params[:id])
     end
 end
--- voice-recording/config/routes.rb
+++ voice-recording/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "/users/:id" => 'users#update'
   resources :users
 
   devise_for :users
